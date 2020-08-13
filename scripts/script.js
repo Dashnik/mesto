3d28cb97f83b843b31b79bcf6cd0720405a54847
Alexey Kovalev <Alexey.Kovalev@kofax.com>
2020-08-13 12:27:43 +0300

    fix: popup will able to close and open

--- scripts/script.js
+++ scripts/script.js
@@ -2,11 +2,16 @@ const popup = document.querySelector('.popup');
 const popupOpenButton = document.querySelector('.profile__name-edit_open-popup');
 const popupCloseButton = popup.querySelector('.popup__close');
 
-const popupToggle = function (){
-
+const popupToggle = function (event){
+  event.preventDefault();
   popup.classList.toggle('popup_opened');
+}
 
+const closePopup = function (event) {
+  if (event.target !== event.currentTarget) return
+  popupToggle(event);
 }
 
 popupOpenButton.addEventListener('click',popupToggle );
 popupCloseButton.addEventListener('click',popupToggle );
+popup.addEventListener('click', closePopup);
