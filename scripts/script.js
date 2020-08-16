7d996dcc63c5f4fef74d690ca87ca2eb9979d87b
Alexey Kovalev <Alexey.Kovalev@kofax.com>
2020-08-16 14:17:12 +0300

    feat: value from profile__name and description were inserted on popup. Sprint5.Project4

--- scripts/script.js
+++ scripts/script.js
@@ -15,3 +15,48 @@ const closePopup = function (event) {
 popupOpenButton.addEventListener('click',popupToggle );
 popupCloseButton.addEventListener('click',popupToggle );
 popup.addEventListener('click', closePopup);
+
+//установка значений для полей в попапе
+
+//Найти нужное поле на странице
+let ProfileName = document.querySelector('.profile__name');
+//извлечь оттуда текст и записать в переменную
+let textProfileName = ProfileName.textContent;
+//найти поле в которое хочешь записать значение на попапе
+let nameInput = document.querySelector('.popup__name'); 
+//Присвоить текстовое значение в атрибут value в input
+nameInput.setAttribute('value', textProfileName);
+
+
+//Найти нужное поле на странице
+let ProfileDescription = document.querySelector('.profile__description');
+//извлечь оттуда текст и записать в переменную
+let textProfileDescription = ProfileDescription.textContent;
+//найти поле в которое хочешь записать значение на попапе
+let jobInput = document.querySelector('.popup__description'); 
+//Присвоить текстовое значение в атрибут value в input
+jobInput.setAttribute('value', textProfileDescription);
+
+
+/*
+// Обработчик «отправки» формы, хотя пока
+// она никуда отправляться не будет
+function formSubmitHandler (evt) {
+    evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
+                        // Так мы можем определить свою логику отправки.
+                        // О том, как это делать, расскажем позже.
+
+    // Находим поля формы в DOM
+    let nameInput = document.querySelector('.popup__name'); // Воспользуйтесь инструментом .querySelector()
+    let jobInput = document.querySelector('.popup__description');// Воспользуйтесь инструментом .querySelector()
+
+    // Получите значение полей из свойства value
+
+    // Выберите элементы, куда должны быть вставлены значения полей
+
+    // Вставьте новые значения с помощью textContent
+}
+
+// Прикрепляем обработчик к форме:
+// он будет следить за событием “submit” - «отправка»
+popup.addEventListener('submit', formSubmitHandler);*/
