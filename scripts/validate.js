120043f008d182c8cce6ba5b2ca037ba187e8177
Alexey Kovalev <Alexey.Kovalev@kofax.com>
2020-10-05 01:03:25 +0300

    feature validate.js file is created.enableValidation, setEventListeners, toggleButtonState, CheckInputValidity, Show/hideinputError methods are created

--- scripts/validate.js
+++ scripts/validate.js
@@ -1,52 +1,71 @@
-// // Функция, которая добавляет класс с ошибкой
-// const showInputError = (formElement,inputElement, errorMessage) => {
-//     const errorElement = formElement.querySelector(`#${inputElement.id}-error`);
-  
-//     errorMessage.textContent = errorMessage;
-//     errorMessage.classList.add('form__input-error_active');
-// }
+// Функция, которая добавляет класс с ошибкой
+const showInputError = (formElement, inputElement, errorMessage) => {
+  const errorElement = formElement.querySelector(`#${inputElement.id}-error`);
+  console.log(errorElement);
+
+  errorElement.textContent = errorMessage;
+  errorElement.classList.add("popup__input-error_active");
+};
+
+// Функция, которая удаляет класс с ошибкой
+const hideInputError = (formElement, inputElement) => {
+  const errorElement = formElement.querySelector(`#${inputElement.id}-error`);
+
+  errorElement.textContent = "";
+  errorElement.classList.remove("popup__input-error_active");
+};
+
+// Функция, которая проверяет валидность поля
+const checkInputValidity = (formElement, inputElement) => {
+  const isInputNotValid = !inputElement.validity.valid;
+
+  if (isInputNotValid) {
+    const errorMessage = inputElement.validationMessage;
+    // Если поле не проходит валидацию, покажем ошибку
+    showInputError(formElement, inputElement, errorMessage);
+  } else {
+    // Если проходит, скроем
+
+    hideInputError(formElement, inputElement);
+  }
+};
+
+const toggleButtonState = (inputList, buttonElement) =>{
+    
+    const hasInValidInput = inputList.some((inputElement) => !inputElement.validity.valid);
+    console.log(hasInValidInput);
+
+    if(hasInValidInput){
+        buttonElement.classList.add('popup__submit_inactive');
+        buttonElement.setAttribute('disabled',true);
+    }    
+    else{
+        buttonElement.classList.remove('popup__submit_inactive');
+        buttonElement.removeAttribute('disabled',true);
+    }
+    }
+
+const setEventListeners = (formElement) => {
+  const inputList = Array.from(formElement.querySelectorAll(".popup__input"));
+  const buttonElement = formElement.querySelector('.popup__submit');
+
+  inputList.forEach((inputElement) => {
+    inputElement.addEventListener("input", () => {
+      checkInputValidity(formElement, inputElement);
+      toggleButtonState(inputList, buttonElement);
+    });
+  });
+};
 
 
-// // Функция, которая удаляет класс с ошибкой
-// const hideInputError = (formElement,inputElement) => {
-//     const errorElement = formElement.querySelector(`#${inputElement.id}-error`);
-  
-//     errorMessage.textContent = '';
-//     errorMessage.classList.remove('form__input-error_active');
-// };
+const enableValidation = () => {
+  const formList = Array.from(document.querySelectorAll(".popup"));
 
-// // Функция, которая проверяет валидность поля
-// const checkInputValidity = (formElement,inputElement) => {
-//     const isInputNotValid = !inputElement.validity.valid;
-
-//   if (isInputNotValid) {
-//     // Если поле не проходит валидацию, покажем ошибку
-//     showInputError(formElement,inputElement, errorMessage);
-//   } else {
-//     // Если проходит, скроем
-//     hideInputError(formElement,inputElement);}
-  
-// };
-
-// const setEventListeners = (formElement)=>{
-//     const inputList = Array.from(formElement.querySelectorAll('.popup__input'));
-
-//     inputList.forEach((inputElement) =>{
-//         inputElement.addEventListener('input', ()=>{
-          
-//             checkInputValidity(formElement, inputElement);
-//         });
-//     });
-// };
-
-// const enableValidation = () =>{
-//     const formList = Array.from(document.querySelectorAll('.popup'));
-
-// formList.forEach((formElement) =>{
-//     formElement.addEventListener('submit', (event)=>{
-//         event.preventDefault();
-//     });
-//     setEventListeners(formElement);
-// });
-// };
-// enableValidation();
+  formList.forEach((formElement) => {
+    formElement.addEventListener("submit", (event) => {
+      event.preventDefault();
+    });
+    setEventListeners(formElement);
+  });
+};
+enableValidation();
