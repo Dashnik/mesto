def081d629f2bf10c5465dbd6e0ba2d6c1a9d16a
Alexey Kovalev <Alexey.Kovalev@kofax.com>
2020-12-17 13:17:37 +0300

    Feature:profile__photo_container css is implemented

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -6,7 +6,7 @@ import PopupWithImage from '../components/PopupWithImage.js';
 import PopupWithForm from '../components/PopupWithForm.js';
 import ApproveForm from '../components/ApproveForm.js';
 import UserInfo from '../components/UserInfo.js';
-import {initialCards} from './Defaults-cards.js';
+//import {initialCards} from './Defaults-cards.js';
 import {validationConfig} from './constants.js';
 import Api from '../components/Api.js';
 
@@ -23,13 +23,14 @@ const popupEditProfileSelector = '.popup_edit-user-profile';
 const popupCardsSelector = ".popup_cards";
 const popupImageSelector = ".popup_image";
 const popupTrashSelector = ".popup_removing_card";
-const popupEditProfilePhotoSelector = ".popup_editing_photo_profile";
+ const popupEditProfilePhotoSelector = ".popup_editing_photo_profile";
 
 const profileNameInput = document.querySelector(".popup__item_profile_name");
 const profileJobInput = document.querySelector(".popup__item_profile_job");
 const addingInactiveClassForSubmit = document.querySelector('.popup_cards__submit');
 
 const profileImage = document.querySelector(".profile__image");
+const profilePhotoContainer = document.querySelector(".profile__photo_container");
 
  const apiProfile = new Api({
    baseUrl: "https://mesto.nomoreparties.co/v1/cohort-17/users/me",
@@ -51,7 +52,8 @@ const profileImage = document.querySelector(".profile__image");
 
  popupWithEditPhoto.setEventListeners();
 
- profileImage.addEventListener('click', function(){
+//  profileImage.addEventListener('click', function(){
+  profilePhotoContainer.addEventListener('click', function(){
   popupWithEditPhoto.open();
  });
 
@@ -63,7 +65,7 @@ const profileImage = document.querySelector(".profile__image");
  });
  
  function handleEditPhotoProfileSubmit(newLink){
-  apiAvatar.changeAvatar(newLink);
+ apiAvatar.changeAvatar(newLink);
   //apiAvatar.changeAvatar(newLink.avatar);
    profileImage.src = newLink.avatar;
  }
